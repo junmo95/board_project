54ad6e6ec5d30d432ba7f1f4daf1b99790e286ad
junmo95 <97028441+junmo95@users.noreply.github.com>
2022-12-20 23:15:24 +0900

    #10 - jpa test 작성 기본 crud + 연관관계 매핑과 cascading 이 잘 동작하는지 볼라고 작성 테스트 데이터와 관련 있기 때문에, 이대로는 실패한다.

---
+++ project-board/src/test/java/com/junmo/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,97 @@
+package com.junmo.projectboard.repository;
+
+import com.junmo.projectboard.config.JpaConfig;
+import com.junmo.projectboard.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+//import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+//@ActiveProfiles("testdb") - 내가 설정한 mysql 설정 쓸라면 이거랑 아래꺼 써서 설정(yaml도 설정필요)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 이렇게 써도 되고 아니면 yaml 에서 전역설정(여기선 이걸로)
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenASelecting_thenWorksFine() {
+        //Given
+
+        //When
+        List<Article> articles = articleRepository.findAll();
+
+        //Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(123);
+    }
+
+    @DisplayName("insert 테스트")
+    @Test
+    void givenTestData_whenAInserting_thenWorksFine() {
+        //Given
+        long previousCount = articleRepository.count();
+
+        //When
+        Article saveArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
+
+        //Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+    }
+
+    @DisplayName("update 테스트")
+    @Test
+    void givenTestData_whenAUpdating_thenWorksFine() {
+        //Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updateHashtag = "#springboot";
+        article.setHashtag(updateHashtag);
+
+        //When
+        Article savedArticle = articleRepository.saveAndFlush(article);
+
+
+        //Then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updateHashtag);
+    }
+
+    @DisplayName("delete 테스트")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine() {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentsSize = article.getArticleComments().size();
+
+        // When
+        articleRepository.delete(article);
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
+    }
+
+}
