ebf06494b06d663defe5c81076ca57efb331ca9b
junmo95 <97028441+junmo95@users.noreply.github.com>
2023-01-01 21:15:17 +0900

    #1 - data rest 테스트 MockMvc.content 메소드 찾을 수 없음 애초에 필수 테스트 아니니 신경쓰지 말자

---
+++ project-board/src/test/java/com/junmo/projectboard/controller/DataRestTest.java
@@ -0,0 +1,89 @@
+package com.junmo.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Data Rest - API테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+    @DisplayName("[api] 게시글 리스트 조회")
+    @Test
+    void giveNothing_whenRequestingArticles_thenReturnsArticlesJsonResponse() throws Exception {
+        //Given
+
+        //When & Then
+        mvc.perform(get("/api/articles")) // MockMvcRequestBuilders.get 을 이용한거다. alt + enter로 정적으로 가져옴
+                .andExpect(status().isOk()) // 존재여부 확인
+//                .andExpect(content.contentType(MediaType.valueOf("application/hal+json")))
+                ;
+    }
+
+    @DisplayName("[api] 게시글 단건 조회")
+    @Test
+    void giveNothing_whenRequestingArticle_thenReturnsArticlesJsonResponse() throws Exception {
+        //Given
+
+        //When & Then
+        mvc.perform(get("/api/articles/1")) // MockMvcRequestBuilders.get 을 이용한거다. alt + enter로 정적으로 가져옴
+                .andExpect(status().isOk()) // 존재여부 확인
+//                .andExpect(content.contentType(MediaType.valueOf("application/hal+json")))
+        ;
+    }
+
+    @DisplayName("[api] 게시글 -> 댓글 조회")
+    @Test
+    void giveNothing_whenRequestingArticleCommentsFromArticle_thenReturnsArticlesJsonResponse() throws Exception {
+        //Given
+
+        //When & Then
+        mvc.perform(get("/api/articles/1/articleComments")) // MockMvcRequestBuilders.get 을 이용한거다. alt + enter로 정적으로 가져옴
+                .andExpect(status().isOk()) // 존재여부 확인
+//                .andExpect(content.contentType(MediaType.valueOf("application/hal+json")))
+        ;
+    }
+
+    @DisplayName("[api] 댓글 리스트 조회")
+    @Test
+    void giveNothing_whenRequestingArticleComments_thenReturnsArticlesJsonResponse() throws Exception {
+        //Given
+
+        //When & Then
+        mvc.perform(get("/api/articleComments")) // MockMvcRequestBuilders.get 을 이용한거다. alt + enter로 정적으로 가져옴
+                .andExpect(status().isOk()) // 존재여부 확인
+//                .andExpect(content.contentType(MediaType.valueOf("application/hal+json")))
+        ;
+    }
+
+    @DisplayName("[api] 댓글 단건 조회")
+    @Test
+    void giveNothing_whenRequestingArticleComment_thenReturnsArticlesJsonResponse() throws Exception {
+        //Given
+
+        //When & Then
+        mvc.perform(get("/api/articleComments/1")) // MockMvcRequestBuilders.get 을 이용한거다. alt + enter로 정적으로 가져옴
+                .andExpect(status().isOk()) // 존재여부 확인
+//                .andExpect(content.contentType(MediaType.valueOf("application/hal+json")))
+        ;
+    }
+
+}
