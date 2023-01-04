faf81ddcbc1ea158b5cb72dbba1cd4159629bdd7
junmo95 <97028441+junmo95@users.noreply.github.com>
2023-01-05 01:32:51 +0900

    #1 - Querydsl 디펜더시 및 설정 추가 Querydsl 사용할 수 있는 환경을 구성한다. 자동 생성 클래스는 프로젝트에 포함되면 안되니까 gitignore로 무시
    &이전까지의 문제들의 yaml 파일에서 볼 수 있듯이 버전 문제였던 것으로 잠정 결정, 그 부분에 대한 수정 대량
    @gitignore, build gradle 제외는 전부 그 영향

--- project-board/src/main/java/com/junmo/projectboard/repository/ArticleRepository.java
+++ project-board/src/main/java/com/junmo/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,32 @@
 package com.junmo.projectboard.repository;
 
 import com.junmo.projectboard.domain.Article;
+import com.junmo.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,  // 이 엔티티(Article)에 있는 모느 필드에 대한 기본 검색기능 추가해줌
+        QuerydslBinderCustomizer<QArticle> {
+
+    //검색에 대한 세부적인 조건 구현
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true); //현제 이기능에 의해서 모든 검색이 열려있으니 선택적인 부분만 가능하게 제한
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy); //원하는 필드 추가
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // 이걸로 하면 검색시 lie '${}'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // 검색 파라미터 하나, 대소문자 무시, 검색시 like '%${}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // 시간 형식
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
--- project-board/src/test/java/com/junmo/projectboard/controller/DataRestTest.java
+++ project-board/src/test/java/com/junmo/projectboard/controller/DataRestTest.java
@@ -6,12 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Disabled("Spring Data Rest 통합테스트는 불필요하므로 제외시킴")
